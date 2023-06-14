931d84f0f7789d408da242ea7f0bd77a41283828
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-14 14:32:17 +0530

    b1 counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-function App(){
-  return(
-    <h1>hello</h1>
+function App() {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <CounterApp />
+    </>
   );
 }
+
+// UserDefineTag
+function CounterApp() {
+  // Member Function
+  let likeMeAction = () => {
+    // alert();
+    console.log("I m button click");
+  };
+
+  return (
+    <>
+      <input type="button" value="Like Me" onClick={likeMeAction} />
+    </>
+  );
+}
+
 export default App;
