105efcd1c1822b0db4d6b340860155f183f7fba9
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-14 14:54:48 +0530

    body change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 // User Defined Tag: <AppHeader />
 function AppHeader() {
   return (
-    <div className="row bg-light text-light p-3 fs-1">
+    <div className="row bg-success text-light p-3 fs-1">
       <div className="col">Home</div>
       <div className="col">Project</div>
       <div className="col">AboutUs</div>
@@ -24,7 +24,21 @@ function AppHeader() {
 function AppBody() {
   return (
     <>
-      <h1>App Body</h1>
+      <div className="alert alert-success fs-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
+        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
+        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
+        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
+        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
+        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
+        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
+        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
+        ratione adipisci deleniti alias aspernatur voluptatem!
+      </div>
     </>
   );
 }
