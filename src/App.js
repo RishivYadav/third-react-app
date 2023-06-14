8f39d215cefc94b625d492e887d583fd6db7be88
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-14 14:46:49 +0530

    mutli func

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import { useState } from "react";
-
+// User Defined Tag: <App />
 function App() {
   return (
     <>
-      <h1>Counter App</h1>
-      <CounterApp />
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
     </>
   );
 }
 
-// UserDefineTag
-function CounterApp() {
-  // Data Member :: stateless/ stateful
-  // let counter = 1; // stateless
-  let [counter, setCounter] = useState(1); // to create stateful variable + this will help to perform dom operation.
-
-  // Member Function
-  let likeMeAction = () => {
-    counter = counter + 1;
-    console.log("I m button click", counter);
-
-    // Asking REACT TO PERFOMR DOM OEPRATION
-    // Calling setCounter => DOM Operation
-    setCounter(counter);
-  };
-
+// User Defined Tag: <AppHeader />
+function AppHeader() {
   return (
     <>
-      <h1>{counter}</h1>
-      <input type="button" value="Like Me" onClick={likeMeAction} />
+      <h1>Header</h1>
+    </>
+  );
+}
+
+// User Defined Tag: <AppBody />
+function AppBody() {
+  return (
+    <>
+      <h1>App Body</h1>
+    </>
+  );
+}
+
+// User Defined Tag: <AppFooter />
+function AppFooter() {
+  return (
+    <>
+      <h1>App Footer</h1>
     </>
   );
 }
